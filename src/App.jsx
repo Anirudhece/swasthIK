7e1fe046abd89384d4720a16431a84bcc1b605e1
Anirudh <anirudhece2020@gmail.com>
2023-09-13 13:10:35 +0530

    some minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SignIN from "./scenes/signIN-signUP/SignIN";
 import SignUP from "./scenes/signIN-signUP/SignUP";
 import ResetPassword from "./scenes/signIN-signUP/ResetPassword";
@@ -6,16 +6,40 @@ import VerifyOTP from "./scenes/signIN-signUP/VerifyOTP";
 import Navbar from "./scenes/global/Navbar";
 import Footer from "./scenes/global/Footer";
 import Maps from "./components/Maps";
+import Home from "./scenes/home/Home";
+
 function App() {
+  const [marginLeft, setMarginLeft] = useState("240px");
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 768) {
+        setMarginLeft("0px");
+      } else {
+        setMarginLeft("240px");
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Call it initially
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
-      {/* <SignIN />
-      <SignUP />
-      <ResetPassword />
-      <VerifyOTP/> */}
-      <Maps />
-      <Footer />
+      <div style={{ marginLeft }}>
+        <Home />
+        {/* <SignIN />
+        <SignUP />
+        <ResetPassword />
+        <VerifyOTP /> */}
+        {/* <Maps /> */}
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/scenes/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Feature from "./components/Features";
+import Topheading from "./components/Topheading";
+
+function Home() {
+  return (
+    <div>
+      <Topheading />
+      <Feature />
+    </div>
+  );
+}
+
+export default Home;
